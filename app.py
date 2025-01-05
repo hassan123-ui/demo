fb9f49fddddbcbd2c5b2d026474629326c1d004d
hassan123-ui <hr5279769@gmail.com>
2025-01-05 19:14:33 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+print("hassan")                                        
+name:str="hassan"
+age:int=19
+is_active:bool=True
+print(is_active)
+print(7+5)
+num1:int=5
+num2:int=5
+sum:int=num1+num2
+print(sum)
+mul:int=num1*num2
+print(mul)
+num1:int=5
+num2:int=5
+
+x=10
+print(x)
+x=10+15+5
+print(x)
+x=x-5
+print(x)
+x=x*2
+print(x)
+x=x/3
+print(x)
+# == l(equal)
+# > (greater than)
+# < (less than)
+# >=(greater than or equal to)
+# <=(less than or equal to)
+print(10==10)
+print(10>15)
+print(5<3)
+num1=25
+num2=30
+print(num1!=num2)
+print(num1<=num2)
+#arthmatic
+#assigement
+#compraison
+#logical operator
+num1=15
+num2=30
+print(num1<10 and num2>35)
+print(num1<10 or num2>35)
+print(num1<10 or num2>35)
+print(num2<35 and num1>10)
+k_electric="no k_electric"
+ups="no ups"
+print(k_electric or ups) 
+num1=50 
+num2=30
+num3=40
+print(num1-num2+num3)
+x = 5 
+y = 3
+print(x**y)
+u=15
+v=20
+sum=u+v
+print(sum)
+x=10+5+15-20+5
+print(x)
+k_electric="on light"
+ups="off light"
+print(k_electric or ups)
+ups="off light"
+k_electric="on light"
+print(ups or k_electric)
+x=2*2^3+10+50
+print(x)
+y=2+3^2
+print(y)
+a=54 
+b=56
+print(a+b)
+a=45
+b=75
+print(a+b)
+print(a*b)
+
+
+
